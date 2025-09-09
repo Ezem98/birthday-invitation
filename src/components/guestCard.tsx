64217802257336c3309e18f6d66edef32f86655f
Ezem98 <56078650+Ezem98@users.noreply.github.com>
2025-09-09 19:43:11 -0300

    disclaimer

--- src/components/guestCard.tsx
+++ src/components/guestCard.tsx
@@ -16,9 +16,12 @@ export default function GuestCard() {
   if (!guest) return NotFoundPage();
 
   return (
-    <div className="w-full h-screen overflow-hidden">
+    <div className="w-full h-screen overflow-hidden relative">
       <Lanyard {...LANYARD_PROPS_DESKTOP} cardGLB={guest.glb} />
       <Lanyard {...LANYARD_PROPS_MOBILE} cardGLB={guest.glb} />
+      <footer className="w-full h-16 text-amber-[#f5e9da] font-bold absolute bottom-0 flex items-center justify-center">
+        <p>Tocar rápido dos veces la tarjeta para ver el detalle.</p>
+      </footer>
     </div>
   );
 }
